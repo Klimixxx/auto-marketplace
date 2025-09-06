e4eaf4b046da98813efaf7ce81a2b51a27afad43
Klimixxx <156718095+Klimixxx@users.noreply.github.com>
2025-09-06 18:01:05 +0300

    Create inspections.js

---
+++ frontend/pages/inspections.js
@@ -0,0 +1,11 @@
+// pages/inspections.js
+export default function Inspections() {
+  return (
+    <div className="container" style={{ maxWidth: 800 }}>
+      <h1>Мои Осмотры</h1>
+      <div className="card" style={{ padding: 16 }}>
+        Раздел скоро появится.
+      </div>
+    </div>
+  );
+}
